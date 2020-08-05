74c65bcf1a179c712e38549004ef4997f0b2c7ea
641589523 <641589523@qq.com>
2020-08-05 21:29:16 +0800

    no message

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,90 +1,101 @@
 //index.js
+import {
+	request
+} from "../../request/index.js"
+
 //获取应用实例
 const app = getApp()
 Page({
 
-	/**
-	 * 页面的初始数据
-	 */
-	data: {
-		swiperList:[],
-		a:1123
-	},
+			/**
+			 * 页面的初始数据
+			 */
+			data: {
+				swiperList: [],
+				a: 1123
+			},
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
-		// 发送异步请求获取轮播图数据
-		var that = this
-		wx.request({
-			url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-			header:{'content-type':'application/json'},
-			method:'GET',
-			dataType:'json',
-			responseType:'text',
-			success:(result)=>{
-					this.setData({
-						swiperList:result.data.message
+			/**
+			 * 生命周期函数--监听页面加载
+			 */
+			onLoad: function (options) {
+				// 发送异步请求获取轮播图数据
+				request({
+						url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+					}).then(result => {
+						this.setData({
+							swiperList:result.data.message
+						})
 					})
-					console.log(123)
-					console.log(this.data.swiperList)
-			},
-			fail:()=>{
+				},
+					// 	wx.request({
+					// 		url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+					// 		header:{'content-type':'application/json'},
+					// 		method:'GET',
+					// 		dataType:'json',
+					// 		responseType:'text',
+					// 		success:(result)=>{
+					// 				this.setData({
+					// 					swiperList:result.data.message
+					// 				})
+					// 				console.log(123)
+					// 				console.log(this.data.swiperList)
+					// 		},
+					// 		fail:()=>{
 
-			},
-			complete:()=>{
+					// 		},
+					// 		complete:()=>{
 
-			}
-		})
-	},
+					// 		}
+					// 	})
+					// },
 
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-		
-	},
+					/**
+					 * 生命周期函数--监听页面初次渲染完成
+					 */
+					onReady: function () {
 
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-		
-	},
+					},
 
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-		
-	},
+					/**
+					 * 生命周期函数--监听页面显示
+					 */
+					onShow: function () {
 
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-		
-	},
+					},
 
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-		
-	},
+					/**
+					 * 生命周期函数--监听页面隐藏
+					 */
+					onHide: function () {
 
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-		
-	},
+					},
 
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-		
-	}
-})
+					/**
+					 * 生命周期函数--监听页面卸载
+					 */
+					onUnload: function () {
+
+					},
+
+					/**
+					 * 页面相关事件处理函数--监听用户下拉动作
+					 */
+					onPullDownRefresh: function () {
+
+					},
+
+					/**
+					 * 页面上拉触底事件的处理函数
+					 */
+					onReachBottom: function () {
+
+					},
+
+					/**
+					 * 用户点击右上角分享
+					 */
+					onShareAppMessage: function () {
+
+					}
+			})
