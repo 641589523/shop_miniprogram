40b6fa376be8f50477a6d21189f1a719a5908f87
xq <3451511593@qq.com>
2020-08-05 14:23:58 +0800

    11
    111

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,37 +2,67 @@
 //获取应用实例
 const app = getApp()
 Page({
-	data:{
+
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
 		
 	},
-	onLoad:function(options){
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
 		
 	},
-	onReady:function(){
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
 		
 	},
-	onShow:function(){
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {
 		
 	},
-	onHide:function(){
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
 		
 	},
-	onUnload:function(){
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
 		
 	},
-	onPullDownRefresh:function(){
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {
 		
 	},
-	onReachBottom:function(){
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {
 		
 	},
-	onShareAppMessage:function(){
-		
-	},
-	onPageScroll:function(){
-		
-	},
-	onTabItemTap:function(item){
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
 		
 	}
-});
+})
