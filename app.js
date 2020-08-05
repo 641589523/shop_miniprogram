cb520bb05a30fbb297fe756137ae20815cb1edbb
xq <3451511593@qq.com>
2020-08-05 11:08:53 +0800

    'baseresources'

---
+++ app.js
@@ -0,0 +1,21 @@
+//app.js
+App({
+	onLaunch:function(options){
+		
+	},
+	onShow:function(options){
+		
+	},
+	onHide:function(options){
+		
+	},
+	onError:function(options){
+		
+	},
+	onPageNotFound:function(options){
+		
+	},
+	globalData:{
+		
+	}
+});
---
+++ pages/index/index.js
@@ -0,0 +1,38 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+	data:{
+		
+	},
+	onLoad:function(options){
+		
+	},
+	onReady:function(){
+		
+	},
+	onShow:function(){
+		
+	},
+	onHide:function(){
+		
+	},
+	onUnload:function(){
+		
+	},
+	onPullDownRefresh:function(){
+		
+	},
+	onReachBottom:function(){
+		
+	},
+	onShareAppMessage:function(){
+		
+	},
+	onPageScroll:function(){
+		
+	},
+	onTabItemTap:function(item){
+		
+	}
+});
