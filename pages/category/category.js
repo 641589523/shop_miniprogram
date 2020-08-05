adc63c8ed660aa8f492ad095e8d2e744fbd851fb
641589523 <641589523@qq.com>
2020-08-05 22:41:50 +0800

    no message

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,18 +1,25 @@
 // pages/category/category.js
+import {
+  request
+} from "../../request/index.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 左侧的菜单数据
+    leftMenuList: [],
+    // 右侧的菜单数据
+    rightContent: []
   },
-
+  // 接口的返回数据
+  Cates: [],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCates()
   },
 
   /**
@@ -62,5 +69,21 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  /**
+   * 获取分类信息
+   */
+  getCates() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/categories'
+    }).then(result => {
+      this.Cates = result.data.message
+      let leftMenuList = this.Cates.map(v=>v.cat_name)
+      let rightContent = this.Cates[0].children
+      this.setData({
+        leftMenuList,
+        rightContent
+      })
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,95 +7,130 @@ import {
 const app = getApp()
 Page({
 
-			/**
-			 * 页面的初始数据
-			 */
-			data: {
-				swiperList: [],
-				a: 1123
-			},
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		swiperList: [],
+		catesList: [],
+		floorList: []
+	},
 
-			/**
-			 * 生命周期函数--监听页面加载
-			 */
-			onLoad: function (options) {
-				// 发送异步请求获取轮播图数据
-				request({
-						url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
-					}).then(result => {
-						this.setData({
-							swiperList:result.data.message
-						})
-					})
-				},
-					// 	wx.request({
-					// 		url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-					// 		header:{'content-type':'application/json'},
-					// 		method:'GET',
-					// 		dataType:'json',
-					// 		responseType:'text',
-					// 		success:(result)=>{
-					// 				this.setData({
-					// 					swiperList:result.data.message
-					// 				})
-					// 				console.log(123)
-					// 				console.log(this.data.swiperList)
-					// 		},
-					// 		fail:()=>{
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+		this.getSwiperList()
+		this.getCatesList()
+		this.getFloorList()
+	},
 
-					// 		},
-					// 		complete:()=>{
 
-					// 		}
-					// 	})
-					// },
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
 
-					/**
-					 * 生命周期函数--监听页面初次渲染完成
-					 */
-					onReady: function () {
+	},
 
-					},
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {
 
-					/**
-					 * 生命周期函数--监听页面显示
-					 */
-					onShow: function () {
+	},
 
-					},
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
 
-					/**
-					 * 生命周期函数--监听页面隐藏
-					 */
-					onHide: function () {
+	},
 
-					},
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
 
-					/**
-					 * 生命周期函数--监听页面卸载
-					 */
-					onUnload: function () {
+	},
 
-					},
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {
 
-					/**
-					 * 页面相关事件处理函数--监听用户下拉动作
-					 */
-					onPullDownRefresh: function () {
+	},
 
-					},
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {
 
-					/**
-					 * 页面上拉触底事件的处理函数
-					 */
-					onReachBottom: function () {
+	},
 
-					},
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
 
-					/**
-					 * 用户点击右上角分享
-					 */
-					onShareAppMessage: function () {
-
-					}
+	},
+	/**
+	 * 发送异步请求获取轮播图数据
+	 */
+	getSwiperList() {
+		request({
+			url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+		}).then(result => {
+			this.setData({
+				swiperList: result.data.message
 			})
+		})
+
+		// 	wx.request({
+		// 		url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+		// 		header:{'content-type':'application/json'},
+		// 		method:'GET',
+		// 		dataType:'json',
+		// 		responseType:'text',
+		// 		success:(result)=>{
+		// 				this.setData({
+		// 					swiperList:result.data.message
+		// 				})
+		// 				console.log(123)
+		// 				console.log(this.data.swiperList)
+		// 		},
+		// 		fail:()=>{
+
+		// 		},
+		// 		complete:()=>{
+
+		// 		}
+		// 	})
+		// },
+	},
+	/**
+	 * 获取分类菜单图标
+	 */
+	getCatesList() {
+		request({
+			url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'
+		}).then(result => {
+
+			this.setData({
+				catesList: result.data.message
+			})
+		})
+	},
+	/**
+	 * 获取楼层数据
+	 */
+	getFloorList(){
+		request({
+			url:'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'
+		}).then(result=>{
+			this.setData({
+				floorList:result.data.message
+			})
+		})
+	}
+})
