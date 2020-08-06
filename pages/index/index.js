a157895b3322a1913f1a911fcdd3fdcc09e8deaa
xq <3451511593@qq.com>
2020-08-06 08:25:25 +0800

    '1'

--- pages/index/index.js
+++ pages/index/index.js
@@ -19,7 +19,7 @@ Page({
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
-	onLoad: function (options) {
+	onLoad: function(options) {
 		this.getSwiperList()
 		this.getCatesList()
 		this.getFloorList()
@@ -29,49 +29,49 @@ Page({
 	/**
 	 * 生命周期函数--监听页面初次渲染完成
 	 */
-	onReady: function () {
+	onReady: function() {
 
 	},
 
 	/**
 	 * 生命周期函数--监听页面显示
 	 */
-	onShow: function () {
+	onShow: function() {
 
 	},
 
 	/**
 	 * 生命周期函数--监听页面隐藏
 	 */
-	onHide: function () {
+	onHide: function() {
 
 	},
 
 	/**
 	 * 生命周期函数--监听页面卸载
 	 */
-	onUnload: function () {
+	onUnload: function() {
 
 	},
 
 	/**
 	 * 页面相关事件处理函数--监听用户下拉动作
 	 */
-	onPullDownRefresh: function () {
+	onPullDownRefresh: function() {
 
 	},
 
 	/**
 	 * 页面上拉触底事件的处理函数
 	 */
-	onReachBottom: function () {
+	onReachBottom: function() {
 
 	},
 
 	/**
 	 * 用户点击右上角分享
 	 */
-	onShareAppMessage: function () {
+	onShareAppMessage: function() {
 
 	},
 	/**
@@ -79,7 +79,7 @@ Page({
 	 */
 	getSwiperList() {
 		request({
-			url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+			url: 'home/swiperdata'
 		}).then(result => {
 			this.setData({
 				swiperList: result.data.message
@@ -124,12 +124,12 @@ Page({
 	/**
 	 * 获取楼层数据
 	 */
-	getFloorList(){
+	getFloorList() {
 		request({
-			url:'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'
-		}).then(result=>{
+			url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'
+		}).then(result => {
 			this.setData({
-				floorList:result.data.message
+				floorList: result.data.message
 			})
 		})
 	}
--- request/index.js
+++ request/index.js
@@ -1,13 +1,29 @@
+let requestNum = 0;
 export const request = (params) => {
-  return new Promise((resolve, reject) => {
-    wx.request({
-      ...params,
-      success: (result) => {
-        resolve(result)
-      },
-      fail: (err) => {
-        reject(err)
-      }
-    })
-  })
+	// 显示加载中
+	wx.showLoading({
+		title: '加载中',
+		mask: true
+	})
+
+	const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1/"
+	requestNum++
+	return new Promise((resolve, reject) => {
+		wx.request({
+			...params,
+			url: baseUrl + params.url,
+			success: (result) => {
+				resolve(result)
+			},
+			fail: (err) => {
+				reject(err)
+			},
+			complete:()=>{
+				requestNum --
+				if(requestNum == 0){
+					wx.hideLoading()
+				}
+			}
+		})
+	})
 }
